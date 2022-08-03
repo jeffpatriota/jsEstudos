e12ad01d2bd925a32d58ab722841682b68fb380e
jeffpatriota <jefferson.patriota@outlook.com>
2022-08-03 17:45:35 -0300

    estudando objeto

---
+++ objeto/oo.js
@@ -0,0 +1,21 @@
+//CÓDIGO NÃO EXECULTÁVEL !!! 
+
+//Procedural 
+processamento(valor1, valor2, valor3);
+
+//oo
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+//Principios importantes:
+//1. abstraçao
+//2. encapsulamento
+//3. herança(prototype)
+//4. poliformismo
